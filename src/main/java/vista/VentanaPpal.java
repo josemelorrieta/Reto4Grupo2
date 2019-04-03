80ea4b25de4108b5e54790f53842374ad5b25e7a
elorrietaa <alba.rodriguezsa@elorrieta-errekamari.com>
2019-04-03 14:19:32 +0200

    Creado package controlador y clases Controlador y Launcher. Creados paneles principales.

--- src/main/java/vista/VentanaPpal.java
+++ src/main/java/vista/VentanaPpal.java
@@ -1,11 +1,14 @@
 package vista;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import vista.panelCenter.PanelBuscar;
+
 import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
 
 public class VentanaPpal extends JFrame {
 
@@ -13,39 +16,37 @@ public class VentanaPpal extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
 	
-	public PanelBuscar buscar = new PanelBuscar(); 
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPpal frame = new VentanaPpal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	public BorderLayout layout;
+	public PanelNorth pNorth;
+	public PanelCenter pCenter;
+	public PanelSouth pSouth;
 
 	/**
 	 * Create the frame.
 	 */
 	public VentanaPpal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new CardLayout(0, 0));
-		setLocationRelativeTo(null);
-		
-		contentPane.add(buscar);
+		setParametros();
+		instanciarObjetos();
 	}
 
+	private void setParametros() {
+		layout = new BorderLayout(0,0);
+		setLayout(layout);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(800,600));
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private void instanciarObjetos() {
+		pNorth = new PanelNorth();
+		add(pNorth, BorderLayout.NORTH);
+		
+		pCenter = new PanelCenter();
+		add(pCenter, BorderLayout.CENTER);
+		
+		pSouth = new PanelSouth();
+		add(pSouth, BorderLayout.SOUTH);
+	}
 }
