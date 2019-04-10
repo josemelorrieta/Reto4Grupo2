37a3b66db68515d89016a1cf86e145e1a435e2ef
IN1DM3B_10 <IN1DM3B_10@H015112.NET>
2019-04-10 12:34:07 +0200

    Refactor creacion array hoteles con el array de camas para cada habitacion

--- src/test/java/testModelo/TestHabitacion.java
+++ src/test/java/testModelo/TestHabitacion.java
@@ -1,25 +1,31 @@
 package testModelo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import modelo.Cama;
 import modelo.Habitacion;
 import modelo.Mobiliario;
 import modelo.TipoHabitacion;
 
 public class TestHabitacion {
 	
+	private int idHabTest = 1;
 	private int metrosCuadradosTest=20;
 	private Habitacion habitacionTest;
 	private Mobiliario[] arrayMobiliarioTest = {new Mobiliario(),new Mobiliario(),new Mobiliario()};
+	private Cama[] arrayCamasTest = {new Cama()};
 	private TipoHabitacion tipoHabitacionTest=TipoHabitacion.COCINA;
 
 	@Test
 	public void testConstructor() {
-		habitacionTest=new Habitacion(metrosCuadradosTest, arrayMobiliarioTest,tipoHabitacionTest);
+		habitacionTest=new Habitacion(idHabTest, metrosCuadradosTest, arrayMobiliarioTest, arrayCamasTest,tipoHabitacionTest);
+		assertEquals(habitacionTest.getIdHab(), idHabTest);
 		assertEquals(habitacionTest.getM2(), metrosCuadradosTest);
 		assertArrayEquals(habitacionTest.getArrayMobiliario(), arrayMobiliarioTest);
+		assertArrayEquals(habitacionTest.getArrayCamas(), arrayCamasTest);
 		assertEquals(habitacionTest.getTipoHabitacion(), tipoHabitacionTest);
 	}
 	
@@ -28,6 +34,13 @@ public class TestHabitacion {
 		habitacionTest=new Habitacion();
 		assertEquals(habitacionTest.getM2(), 0);
 		assertArrayEquals(habitacionTest.getArrayMobiliario(), null);
+	}
+	
+	@Test
+	public void testSetId() {
+		habitacionTest=new Habitacion();
+		habitacionTest.setIdHab(idHabTest);
+		assertEquals(habitacionTest.getIdHab(), idHabTest);
 	}
 	
 	@Test
@@ -42,6 +55,13 @@ public class TestHabitacion {
 		habitacionTest=new Habitacion();
 		habitacionTest.setArrayMobiliario(arrayMobiliarioTest);
 		assertArrayEquals(habitacionTest.getArrayMobiliario(), arrayMobiliarioTest);
+	}
+	
+	@Test
+	public void testSetArrayCamas() {
+		habitacionTest=new Habitacion();
+		habitacionTest.setArrayCamas(arrayCamasTest);
+		assertArrayEquals(habitacionTest.getArrayCamas(), arrayCamasTest);
 	}
 	
 	@Test
