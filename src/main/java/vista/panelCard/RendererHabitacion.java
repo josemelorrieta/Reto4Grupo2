7c0cf483ce64650575ea4ad8e492bcbbb27fb8a7
elorrietaa <albarodriguezsalvador@gmail.com>
2019-04-11 12:27:55 +0200

    Crear paneles PanelSelHabitacion y RendererHabitacion

---
+++ src/main/java/vista/panelCard/RendererHabitacion.java
@@ -0,0 +1,27 @@
+package vista.panelCard;
+
+import java.awt.Component;
+
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+
+import modelo.Habitacion;
+
+public class RendererHabitacion extends JPanel implements ListCellRenderer<Habitacion>{
+
+	/**
+	 * Create the panel.
+	 */
+	public RendererHabitacion() {
+
+	}
+
+	@Override
+	public Component getListCellRendererComponent(JList<? extends Habitacion> arg0, Habitacion arg1, int arg2,
+			boolean arg3, boolean arg4) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
