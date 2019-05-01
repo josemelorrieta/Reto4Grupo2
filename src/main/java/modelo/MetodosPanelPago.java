c0f910b7d27f621ab721d7812c10185128425e3b
José Manuel Muñoz Barragán <josemanuel.munozba@elorrieta-errekamari.com>
2019-05-01 22:58:44 +0200

    Llamada a procedimiento almacenado en BBDD para insertar reserva en tabla reserva y rsvhab

--- src/main/java/modelo/MetodosPanelPago.java
+++ src/main/java/modelo/MetodosPanelPago.java
@@ -162,10 +162,6 @@ public class MetodosPanelPago {
 				panel.modeloCambio.addElement(val);
 			if(!guardarReserva(mod.reserva))
 				JOptionPane.showMessageDialog(panel, "Error al guardar la reserva en la base de datos", "Error", JOptionPane.ERROR_MESSAGE);
-//			if (mod.reserva.getAlojReservado().getClass() == Hotel.class) {
-//				if(!guardarHabReserva(mod.reserva))
-//					JOptionPane.showMessageDialog(panel, "Error al guardar la reserva en la base de datos", "Error", JOptionPane.ERROR_MESSAGE);
-//			}
 		} else {
 			panel.textAPagar.setText(arrDinero[0]);
 			panel.textPagado.setText(arrDinero[1]);
@@ -240,24 +236,16 @@ public class MetodosPanelPago {
 	 * 
 	 */
 	public boolean guardarReserva(Reserva reserva) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		int idRsv = ultimoNumReserva() + 1;
 		String dni = reserva.getCliente().getDni();
-		Date fechaRsv = reserva.getFechaReserva();
-		Date fechaIn = reserva.getFechaEntrada();
-		Date fechaOut = reserva.getFechaSalida();
+		String fechaRsv = sdf.format(reserva.getFechaReserva());
+		String fechaIn = sdf.format(reserva.getFechaEntrada());
+		String fechaOut = sdf.format(reserva.getFechaSalida());
 		double precio = reserva.getPrecio();
 		int idHab = buscarIdHabitacion(reserva.getAlojReservado().getNombre());
 		
 		return bd.guardarReserva(idRsv, dni, fechaRsv, fechaIn, fechaOut, precio, idHab);
-
-	}
-	public boolean guardarReserva2(Reserva reserva) {
-		Object[] objetos = reserva.toObjectArray();
-		
-		int indiceReserva = ultimoNumReserva() + 1;
-		objetos[0] = indiceReserva;
-		
-		return bd.insertGenerico(objetos, "reserva");
 
 	}
 	
